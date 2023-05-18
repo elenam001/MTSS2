7229e85b0c2d07cf460a932f534582f223a5d51d
elenam001 <82274219+elenam001@users.noreply.github.com>
2023-05-18 10:26:50 +0200

    Update IntegerToRomanTest.java

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -1,8 +1,21 @@
 package it.unipd.mtss;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  * Unit test for IntegerToRoman
- */
-public class IntegerToRomanTest {
-    //
+*/
+public class TestIntToRoman {
+
+    /**
+     * Test conversione
+    */
+    @Test
+    public void testScomposizione() {
+        int[] prova = {0,2,3,1};
+        IntegerToRoman.scomponi(231);
+        int[] res = IntegerToRoman.a; 
+        assertArrayEquals(prova, res);
+    }
 }
