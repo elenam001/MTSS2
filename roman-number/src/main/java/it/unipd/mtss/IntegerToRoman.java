780382255da41bbe74acc480ab5bc83c6ffa3a2a
Marco-Favaretto <favaretto801@gmail.com>
2023-05-18 10:18:03 +0200

    set up IntegerToRoman

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,8 +6,24 @@
 package it.unipd.mtss;
 
 public class IntegerToRoman  {
-    public static void main( String[] args )
-    {
-        //
-    }
+    /**
+     * Variabili statiche per indicare la dimensione della cifra
+     */
+    public static final int UNITA = 0;
+    public static final int DECINE = 2;
+    public static final int CENTINAIA = 4;
+    public static final int MIGLIAIA = 6;
+
+    /**
+     * Alfabeto numerazione romana
+     */
+    private static char[] alfabeto = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+
+    /**
+     * array di interi per contenere le cifre del numero da convertire
+     * [migliaia, centinaia, decine, unita]
+     * [   0,         1,        3,     4  ]
+     */
+    public static int a[] = new int[4];
+
 }
