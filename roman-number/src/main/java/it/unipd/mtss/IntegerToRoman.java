fb3fcb5e64fa1ffe4d43925c77f70a9aea672339
Marco-Favaretto <favaretto801@gmail.com>
2023-05-18 10:20:19 +0200

    funzione conversione lettera IntegerToRoman; closes #7

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -25,7 +25,7 @@ public class IntegerToRoman  {
      * [   0,         1,        3,     4  ]
      */
     public static int a[] = new int[4];
-    
+
     /**
      * <p>Assegna ad ogni cella di un array la cifra corrispondente
      * @param num
@@ -42,5 +42,29 @@ public class IntegerToRoman  {
             }
             num/=var;
         }
+    }
+
+    /**
+     * <p>Associa alla cifra e al valore (unita', decine, centinaia, migliaia)
+     *    la/e corrispettiva/e lettera/e della numerazione romana 
+     * @param n cifra da convertire
+     * @param dim odg della cifra da convertire
+     * @return lettera/e della cifra in versione romana
+    */
+    public static String convNumLett(int n, int dim) {
+        String s = "";
+        if(n >= 5) {
+            if(n == 9) { s += alfabeto[dim]; s += alfabeto[dim+2]; }
+            else {
+                s += alfabeto[dim+1];
+                for(int i = 5; i < n; i++) s += alfabeto[dim];
+            }
+        } else {
+            if(n == 4) { s += alfabeto[dim]; s += alfabeto[dim+1]; }
+            else {
+                for(int i = 0; i < n; i++) s += alfabeto[dim];
+            }
+        }
+        return s;
     }
 }
